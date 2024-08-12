0cee0d4e38c4c11ef279d900d43c8a0df0932179
mickeysrishti <Mukeshpathak345@gmail.com>
2024-08-12 23:49:34 +0530

    conditions added

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,14 +1,16 @@
 import "./Product.css"
 
 
-function Product( { title, price, features = []}) {
-  const list = features.map((feature) => <li>{feature}</li>);
+function Product( { title, price }) {
+  // features = [] using as props
+  // const list = features.map((feature) => <li>{feature}</li>);
 
   return (
     <div className="product">
         <h1>{title}</h1>
         <p>{price}</p>
-      <ul>{list}</ul>
+        {price > 100000 ? <p>Discount Of 5%</p> : null}
+      {/* <ul>{list}</ul> */}
 
         
     </div>
