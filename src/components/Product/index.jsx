d8f6c40bc45435d20dcb0fdaeb7e6cd4838ed897
mickeysrishti <Mukeshpathak345@gmail.com>
2024-08-12 23:56:53 +0530

    Dynamic Component Styling

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -4,12 +4,18 @@ import "./Product.css"
 function Product( { title, price }) {
   // features = [] using as props
   // const list = features.map((feature) => <li>{feature}</li>);
+  const isDiscount = price > 100000;
+  const styles = {
+    backgroundColor:  isDiscount? "yellow" : "",
+    color: isDiscount ? "Black" : ""
+
+  };
 
   return (
-    <div className="product">
+    <div className="product" style={styles}>
         <h1>{title}</h1>
         <p>{price}</p>
-        {price > 100000 ? <p>Discount Of 5%</p> : null}
+        {isDiscount ? <p>Discount Of 5%</p> : null}
       {/* <ul>{list}</ul> */}
 
         
